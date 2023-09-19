3c50b4453c6d7652ffc18346211818099c787104
Clara <144264170+clara-tyman@users.noreply.github.com>
2023-09-19 13:28:36 -0400

    typescript version

--- main.ts
+++ main.ts
@@ -1,8 +1,15 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2023 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Clara Tyman
+ * Created on: Sep 2023
+ * This program calculates area and perimeter
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.pause(1000)
+
+basic.showString('A Rectangle has dimensions 5cm & 3cm') 
+
+basic.showString('The perimiter would be' + (5 * 3).toString() + 'cm')
+
+basic.showString('The area would be' + (2 * (5 + 3)).toString() + 'cm^2')
